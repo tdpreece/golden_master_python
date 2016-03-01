156802895605b5ac0fa22516dce33342a98a498e
Tim Preece <tdpreece@gmail.com>
2016-03-01 13:49:18 +0000

    Control random number seed and add expected output as this can now be determined.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import app
 import mock
+import random
 import unittest
 
 from StringIO import StringIO
@@ -8,8 +9,17 @@ from StringIO import StringIO
 class TestApp(unittest.TestCase):
     @mock.patch('sys.stdout', new_callable=StringIO)
     def test(self, stdout):
+        random.seed(0)
         app.main()
-        self.assertEqual(stdout.getvalue(), '')
+        self.assertEqual(
+            stdout.getvalue(),
+            (
+                '0.844421851525\n0.75795440294\n0.420571580831\n'
+                '0.258916750293\n0.511274721369\n0.40493413745\n'
+                '0.783798589035\n0.303312726079\n0.476596954152\n'
+                '0.583382039455\n'
+            )
+        )
 
 
 if __name__ == '__main__':
