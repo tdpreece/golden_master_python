d7fde70825b83adb23caa0488cf2592921178dd5
Tim Preece <tdpreece@gmail.com>
2016-03-01 18:04:01 +0000

    Use approvals framework to assert outputs.

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import mock
 import random
 import unittest
 
+from approvaltests import Approvals
+from approvaltests.TextDiffReporter import TextDiffReporter
 from StringIO import StringIO
 
 
@@ -11,15 +13,8 @@ class TestApp(unittest.TestCase):
     def test(self, stdout):
         random.seed(0)
         app.main()
-        self.assertEqual(
-            stdout.getvalue(),
-            (
-                '0.844421851525\n0.75795440294\n0.420571580831\n'
-                '0.258916750293\n0.511274721369\n0.40493413745\n'
-                '0.783798589035\n0.303312726079\n0.476596954152\n'
-                '0.583382039455\n'
-            )
-        )
+        reporter = TextDiffReporter()
+        Approvals.verify(stdout.getvalue(), reporter)
 
 
 if __name__ == '__main__':
